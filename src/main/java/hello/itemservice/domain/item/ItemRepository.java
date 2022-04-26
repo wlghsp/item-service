55f6b24b880e6a9bacd9d57dfd10b01c7d3b53ad
wlghsp <wlghsp@gmail.com>
2022-04-26 14:24:11 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,42 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository // 들어가면 @component가 있어서 componentscan에 대상이 됨
+public class ItemRepository {
+
+    // 멀티스레드 환경에서는 HashMap을 써서는 안됨. 동시에 여러 스레드가 접근, 그때는 ConcurrentHashMap 사용
+    private static final Map<Long, Item> store = new HashMap<>(); // static
+    private static long sequence = 0L; // static
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
