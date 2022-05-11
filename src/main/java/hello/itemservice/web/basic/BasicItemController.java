8d5d87bceaf9794b7638d3b373ae330fa9123731
wlghsp <wlghsp@gmail.com>
2022-05-11 17:55:50 +0900

    상품등록폼

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -40,6 +41,21 @@ public class BasicItemController {
         model.addAttribute("item", item);
         return "basic/item";
     }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    /**
+     *
+     * 같은 URL을 HTTP 메소드로 구분함.Get or Post
+     */
+    @PostMapping("/add")
+    public String save() {
+        return "basic/addForm";
+    }
+
 
     /**
      * 테스트용 데이터 추가
